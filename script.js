95a568cb5300d2db9b351eb6e114556aa59de232
Ale Candia <alejandrogcandia@gmail.com>
2025-09-10 14:26:36 -0300

    se configura menu hamburguesa, se realiza la configuracion de responsive

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.getElementById('nav-menu');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+  });
+</script>
